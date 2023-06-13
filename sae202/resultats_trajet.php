417102ad6bfbf48a35ec66d6c61f1927f2336dda
Louna Petitfils <60296880+Wysath@users.noreply.github.com>
2023-06-13 16:17:39 +0200

    sae202
    sae202 update trajet

--- sae202/resultats_trajet.php
+++ sae202/resultats_trajet.php
@@ -26,22 +26,57 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         foreach ($resultats as $resultat) {
             echo "Trajet de " . $resultat['depart'] . " à " . $resultat['arrivee'] . "<br>";
             echo "Nom de la personne : " . $resultat['nom'] . "<br>";
-            echo "Nombre de places disponibles : " . $resultat['nombre_places'] . "<br>";
+
+            // Requête SQL pour récupérer le nombre de réservations pour ce trajet
+            $trajetId = $resultat['trajet_id'];
+            $sqlReservation = "SELECT COUNT(*) AS reservations FROM Reservations WHERE trajet_id = :trajetId";
+            $stmtReservation = $mabd->prepare($sqlReservation);
+            $stmtReservation->bindParam(':trajetId', $trajetId);
+            $stmtReservation->execute();
+            $reservationResult = $stmtReservation->fetch(PDO::FETCH_ASSOC);
+
+            $placesDisponibles = $resultat['nombre_places'] - $reservationResult['reservations'];
+
+            echo "Nombre de places disponibles : " . $placesDisponibles . "<br>";
+
             echo "Date et heure : " . $resultat['date'] . " à " . $resultat['heure'] . "<br><br>";
 
+            // Requête SQL pour récupérer les détails des réservations pour ce trajet
+            $sqlReservationsTrajet = "SELECT Reservations.*, Usagers.prenom 
+                                      FROM Reservations 
+                                      INNER JOIN Usagers ON Reservations.user_id = Usagers.user_id
+                                      WHERE trajet_id = :trajetId";
+            $stmtReservationsTrajet = $mabd->prepare($sqlReservationsTrajet);
+            $stmtReservationsTrajet->bindParam(':trajetId', $trajetId);
+            $stmtReservationsTrajet->execute();
+            $reservationsTrajet = $stmtReservationsTrajet->fetchAll(PDO::FETCH_ASSOC);
+
+            if (count($reservationsTrajet) > 0) {
+                echo "Personnes ayant déjà réservé :<br>";
+
+                foreach ($reservationsTrajet as $reservation) {
+                    echo $reservation['prenom'] . "<br>";
+                }
+            } else {
+                echo "Aucune réservation pour le moment.<br>";
+            }
+
             // Formulaire de réservation
-            echo '<form method="post" action="reservation.php">';
-            echo '<input type="hidden" name="trajet_id" value="' . $resultat['trajet_id'] . '">';
-            echo '<input type="submit" value="Réserver">';
-            echo '</form>';
+            if ($placesDisponibles > 0) {
+                echo '<form method="post" action="reservation.php">';
+                echo '<input type="hidden" name="trajet_id" value="' . $resultat['trajet_id'] . '">';
+                echo '<input type="submit" value="Réserver">';
+                echo '</form>';
+            } else {
+                echo 'Complet<br><br>';
+            }
+
             echo "<br><br>";
         }
     } else {
         echo "Aucun trajet disponible pour cette recherche.";
     }
-
 }
-
 ?>
 
 </body>
