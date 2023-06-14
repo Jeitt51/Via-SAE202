5062f86d63bc84a55375f758b2d64e2c1cfe7ea9
Louna Petitfils <60296880+Wysath@users.noreply.github.com>
2023-06-14 17:52:08 +0200

    update pop up creer trajet verif

--- sae202/creer_trajet.php
+++ sae202/creer_trajet.php
@@ -1,10 +1,49 @@
 <?php
-
 // Connexion à la base de données
 require 'lib.inc.php';
-
 $mabd = connexionBD();
 
+// Vérification si le formulaire de création de trajet a été soumis
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Récupération des données du formulaire
+    $depart = $_POST['depart'];
+    $arrivee = $_POST['arrivee'];
+    $date = $_POST['date'];
+    $heure = $_POST['heure'];
+    $nombrePlaces = $_POST['nombre_places'];
+
+    // Vérification si l'utilisateur est connecté
+    session_start();
+    if (isset($_SESSION['user_id'])) {
+        // Récupération de l'ID de l'utilisateur connecté
+        $user_id = $_SESSION['user_id'];
+
+        // Requête SQL pour insérer le nouveau trajet dans la table "Trajets"
+        $sqlInsert = "INSERT INTO Trajets (user_id, depart, arrivee, date, heure, nombre_places) VALUES (:user_id, :depart, :arrivee, :date, :heure, :nombre_places)";
+        $stmt = $mabd->prepare($sqlInsert);
+        $stmt->bindValue(':user_id', $user_id);
+        $stmt->bindValue(':depart', $depart);
+        $stmt->bindValue(':arrivee', $arrivee);
+        $stmt->bindValue(':date', $date);
+        $stmt->bindValue(':heure', $heure);
+        $stmt->bindValue(':nombre_places', $nombrePlaces);
+        $stmt->execute();
+
+        // Vérification si l'insertion a réussi
+        if ($stmt->rowCount() > 0) {
+            // Récupération de l'ID du trajet inséré
+            $trajet_id = $mabd->lastInsertId();
+            echo "<script>alert('Le trajet a été créé avec succès.');</script>";
+            // Vous pouvez effectuer d'autres actions ici si nécessaire
+        } else {
+            echo "<script>alert('Erreur lors de la création du trajet.');</script>";
+            // Vous pouvez afficher un message d'erreur ou effectuer d'autres actions ici si nécessaire
+        }
+    } else {
+        echo "<script>alert('Utilisateur non connecté.');</script>";
+        // Vous pouvez afficher un message d'erreur ou effectuer d'autres actions ici si nécessaire
+    }
+}
 
 // Récupération des options de départ et d'arrivée depuis la base de données
 $optionsDepart = [];
@@ -35,7 +74,7 @@ deconnexionBD($mabd);
 ?>
 
 <!-- Formulaire de création de trajet -->
-<form method="post" action="valid_trajet.php">
+<form method="post" action="">
     <label for="depart">Départ :</label>
     <select id="depart" name="depart" required>
         <option value="">Choisir une option</option>
@@ -67,5 +106,3 @@ deconnexionBD($mabd);
 
     <input type="submit" value="Créer trajet">
 </form>
-</body>
-</html>
